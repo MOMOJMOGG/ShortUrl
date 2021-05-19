fc5020080ef3ee362d864d5f67886138eb1d0279
MOMOJMOGG <momomacaca@gmail.com>
2021-05-19 23:05:16 +0800

    feat: add routes framwork

---
+++ routes/modules/home.js
@@ -0,0 +1,13 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+// 引用 Todo model
+const ShortUrl = require('../../models/shortUrl')
+
+router.get('/', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
