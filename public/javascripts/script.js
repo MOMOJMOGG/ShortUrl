c5ce940f845a9b02281d3a71af300f71dba593d4
MOMOJMOGG <momomacaca@gmail.com>
2021-05-22 16:06:54 +0800

    feat: add copy time

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,23 @@
+function append_zero(element) {
+  if (element < 10) {
+    return '0' + element
+  } else {
+    return '' + element
+  }
+}
+function get_date() {
+  const date = new Date()
+  const hour = date.getHours() + 1 // 0 ~ 23
+  const minutes = date.getMinutes() + 1 // 0 ~ 59
+  const second = date.getSeconds() + 1 // 0 ~ 59
+  return append_zero(hour) + '-' + append_zero(minutes) + '-' + append_zero(second)
+}
+
+
 $(document).ready(() => {
   const copyCode = document.getElementById('short-link')
   $('#copy-btn').click(() => {
+    $('.toast').find('small').text(get_date())
     $('.toast').toast('show')
 
     const selection = window.getSelection()
@@ -11,4 +28,3 @@ $(document).ready(() => {
     document.execCommand('copy')
   })
 })
-
